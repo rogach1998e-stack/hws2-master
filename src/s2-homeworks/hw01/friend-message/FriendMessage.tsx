a8849094dbe7e3a17e16e1690cafe275bff45b9e
Evgen Rog <rogach1998e@gmail.com>
2026-09-06 18:31:29 +0300

    Fix message element dynamic IDs

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -8,23 +8,23 @@ export type FriendMessagePropsType = {
 // создать тип вместо any и отобразить приходящие данные
 const FriendMessage = (props: FriendMessagePropsType) => {
     return (
-        <div id={'hw1-friend-message'} className={s.friendMessage}>
+        <div id={'hw1-friend-message-' + props.message.id} className={s.friendMessage}>
             <div className={s.friendImageAndText}>
                 <img
-                    id={'hw1-friend-avatar-0'}
+                    id={'hw1-friend-avatar-' + props.message.id}
                     src={props.message.user.avatar}
                     alt={'avatar'}
                 />
                 <div className={s.friendText}>
-                    <div id={'hw1-friend-name-0'} className={s.friendName}>
+                    <div id={'hw1-friend-name-' + props.message.id} className={s.friendName}>
                         {props.message.user.name}
                     </div>
-                    <pre id={'hw1-friend-text-0'} className={s.friendMessageText}>
+                    <pre id={'hw1-friend-text-' + props.message.id} className={s.friendMessageText}>
                         {props.message.message.text}
                     </pre>
                 </div>
             </div>
-            <div id={'hw1-friend-time-0'} className={s.friendTime}>
+            <div id={'hw1-friend-time-' + props.message.id} className={s.friendTime}>
                 {props.message.message.time}
             </div>
         </div>
--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -9,23 +9,23 @@ export type MessagePropsType = {
 // нужно отобразить приходящие данные
 const Message = (props: MessagePropsType) => {
     return (
-        <div id={'hw1-message'} className={s.message}>
+        <div id={'hw1-message-' + props.message.id} className={s.message}>
             <div className={s.imageAndText}>
                 <img
-                    id={'hw1-avatar-0'}
+                    id={'hw1-avatar-' + props.message.id}
                     src={props.message.user.avatar}
                     alt={'avatar'}
                 />
                 <div className={s.text}>
-                    <div id={'hw1-name-0'} className={s.name}>
+                    <div id={'hw1-name-' + props.message.id} className={s.name}>
                         {props.message.user.name}
                     </div>
-                    <pre id={'hw1-text-0'} className={s.messageText}>
+                    <pre id={'hw1-text-' + props.message.id} className={s.messageText}>
                         {props.message.message.text}
                     </pre>
                 </div>
             </div>
-            <div id={'hw1-time-0'} className={s.time}>
+            <div id={'hw1-time-' + props.message.id} className={s.time}>
                 {props.message.message.time}
             </div>
         </div>
